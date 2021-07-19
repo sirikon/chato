3eaeb7d00b86b605521d240f3ceb6f0772f0e15b
Carlos Fdez. Llamas <hello@sirikon.me>
2021-07-19 20:03:57 +0200

    remove log color

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -4,10 +4,7 @@ import winston = require('winston')
 export const log = createLogger({
   transports: [
     new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple()
-      )
+      format: winston.format.simple()
     })
   ]
 });
